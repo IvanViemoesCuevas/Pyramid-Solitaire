b95597b4f83a525ab60a14116032fbab5cd865dd
Iván Viemoes Cuevas <111234017+IvanViemoesCuevas@users.noreply.github.com>
2022-09-05 19:37:17 +0200

    Add files via upload

---
+++ Du tabte.vb
@@ -0,0 +1,17 @@
+﻿'**************************************
+'*       Pyramid solitaire            *
+'**************************************
+'* Spillet er en lignende version til *
+'* til den normale kabale af          *
+'* pyramide kabale                    *
+'**************************************
+'* Iván Viemoes Cuevas   27-04-2020   *
+'**************************************
+
+
+Public Class Du_tabte
+    Private Sub Button_TryAgain_Click(sender As Object, e As EventArgs) Handles Button_TryAgain.Click
+        'hvis der trykket på knappen starter den spillet forfra
+        Application.Restart()
+    End Sub
+End Class
---
+++ Du vandt.vb
@@ -0,0 +1,17 @@
+﻿'**************************************
+'*       Pyramid solitaire            *
+'**************************************
+'* Spillet er en lignende version til *
+'* til den normale kabale af          *
+'* pyramide kabale                    *
+'**************************************
+'* Iván Viemoes Cuevas   27-04-2020   *
+'**************************************
+
+
+Public Class Du_vandt
+    Private Sub Button_PlayAgain_Click(sender As Object, e As EventArgs) Handles Button_PlayAgain.Click
+        'hvis der trykket på knappen starter den spillet forfra
+        Application.Restart()
+    End Sub
+End Class
